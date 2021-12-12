69bb6eb721435f342416166a505e360d18f35bb6
RAISA ABDRAI KYZY <raisa.abdraikyzy.kk@gmail.com>
2021-12-13 01:26:05 +0300

    Basic operators

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+//let js = 'amazing';
+//console.log(40 + 8 + 23 - 10);
+
+// Variables and VALUES =smallest unit of information in JS also conventions and rules
+//not allaowed 
+//variables names should be letters with lowercase, numbers $ or _, first symbol should not be a number, uppercase, or any other symbols or reserved names
+let firstName = 'Jonas';
+console.log(firstName);
+
+
+//the 7 primitive data types
+//1. Number floating point numbers used for decimals and integers:    
+let age = 23; 
+
+//2. strings. sequence of characters used for text: ispolsuiutsia vsegda skopki
+let firstName = 'Jones';
+
+//3. boolean logical datatype that can only be true or false used for taking decisions:
+let fullAge = true;
+
+//4. unndefined vlaue taken by variable that is not yet defined ('empty value):
+let children;
+
+//5. Null also means 'empty value
+
+//6.symbol (es2015) value that is unique and cannot be changed (not useful for now);
+
+//7. BigInt (ES2020) larger integers than the number type can hold;
+*/
+
+
+/*
+//VALUE TYPE data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 32);
+//console.log(typeof 'Jones'); //white color always string , string is always with quotes/
+
+javascriptIsFun = 'Yes!'; // changing the value it is dynemic typing
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year); 
+year = 1991;
+console.log(typeof year); 
+
+console.log(typeof null);
+
+
+//3 types of clearing Data types of JS
+let age = 30; // we can change any time
+age = 31;
+
+const birthYear = 1991; // we can not reasign
+birthYear = 1992;
+
+var job = 'programmer'; //same as let but old one
+job = 'teacher';
+
+lastName = 'Schmit'; // never use without variables
+console.log(lastName);
+*/
+
+
+//JS Basic operators
